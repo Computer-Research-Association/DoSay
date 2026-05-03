69f2f42ead3a3b0154f8dc594abeec4e689de818
안희찬 <kashn4655@gmail.com>
2026-05-04 01:27:58 +0900

    feat: add Book model

--- model/BankModel.java
+++ model/BankModel.java
@@ -1,1 +1,36 @@
-"" 
+package model;
+
+
+public class BankModel {
+    public class Book {
+        private String bookname;
+        private String author;
+
+
+        public Book() {}
+
+        public Book(String bookname, String author) {
+            this.bookname = bookname;
+            this.author = author;
+        }
+
+        public String getBookname() {
+            return bookname;
+        }
+
+        public void setBookname(String bookname) {
+            this.bookname = bookname;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+
+    }
+
+}

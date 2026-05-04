11622a1a208bc9877b9fd912df086efa60d992b1
shekqchan <96755597+shekqchan@users.noreply.github.com>
2026-05-04 21:19:14 +0900

    Revert "Feature/setup View and Controller run method"

--- controller/Main.java
+++
@@ -1,8 +0,0 @@
-import controller.BankController;
-
-public class Main {
-    public static void main(String[] args) {
-        BankController controller = new BankController();
-        controller.run();
-    }
-}
